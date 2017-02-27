6ac5734cda393bd6803f936e2ba82cba8a9b835f
sycadidas <sycadidas@163.com>
2017-02-27 17:10:40 +0800

    测试类

--- src/main/java/cn/Test.java
+++ src/main/java/cn/Test.java
@@ -1,12 +1,26 @@
 package cn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
-		String x = "/a/b";
-		String y = "/s";
-		String[] split = y.split("/");
-		System.out.println(split.length);
+		String Categ = "";
+		List<String> l = new ArrayList<String>();
+		l.add("a");
+		l.add("b");
+		l.add("c");
+		l.add("d");
+		l.add("e");
+		
+		if(l.size()>0){
+			for(int i=0;i<l.size()-1;i++){
+					Categ = Categ+l.get(i)+",";
+			}
+			Categ = Categ+l.get(l.size()-1);
+		}
+		System.out.println(Categ);
 	}
 
 }
