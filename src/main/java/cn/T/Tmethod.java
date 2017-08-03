fb7823a38a39b40acf1d6a1e894316c8862b063f
sycadidas <sycadidas@163.com>
2017-08-03 10:13:37 +0800

    泛型与可变参数结合

--- src/main/java/cn/T/Tmethod.java
+++ src/main/java/cn/T/Tmethod.java
@@ -1,5 +1,7 @@
 package cn.T;
 
+import java.util.Random;
+
 
 public class Tmethod {
 /**
@@ -7,17 +9,15 @@ public class Tmethod {
  * 类型参数需要放在修饰符后面、返回值类型前面。一旦定义了类型参数，就可以在参数列表、方法体和返回值类型中使用了。
  */
 	
-	public static <TT,T extends Number> T imTmethod(T t){
+	public static <TT,T extends Number> T imTmethod(Number...t){
 		T l = null;
-		if(t instanceof Object){
-			System.out.println("T");
-		}else{
-			System.out.println("t");
+		for (Number t2 : t) {
+			System.out.println(t2);
 		}
 		
 		return l;
 	}
 	public static void main(String[] args) {
-		System.out.println(imTmethod(5));
+		System.out.println(imTmethod(5,8,9,0));
 	}
 }
